bf7f96095c34d425abf8b3a63a158e92619ed739
Unknown <pratyush.agnihotri@stud.tu-darmstadt.de>
2017-08-02 15:51:40 +0200

    Python Programming
    Using comments, variable scope, keyword, flexible, number, unpacking arguments and dictionary

---
+++ 10_VariableScope/main.py
@@ -0,0 +1,12 @@
+a = 7823
+
+def corn():
+    a = 123
+    print(a, "Inside corn function")
+
+
+def fudge():
+    print(a)
+
+corn()
+fudge()
---
+++ 14_MyTripToWalmartAndSet/main.py
@@ -0,0 +1,7 @@
+groceries = {'cereal', 'milk', 'starcrunch', 'beer', 'duct tpe', 'lotion', 'beer'}
+print(groceries)
+
+if 'milk' in groceries:
+    print("You have already a milk")
+else:
+    print("Oh yea, you need milk")
